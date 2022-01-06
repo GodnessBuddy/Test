25da51e133c5dcbc49ed896ea75424716a11252e
GodnessBuddy <marvin.ruwe@hebk.de>
2022-01-06 16:39:15 +0100

    bis 12

--- java/for_schleife.java
+++ java/for_schleife.java
@@ -1,7 +1,9 @@
 public class for_schleife {
     public static void main(String[] args) {
         
-        
+        for(int i = 5; i < 10; i++){
+            System.out.println(i);
+        }
 
 
 
