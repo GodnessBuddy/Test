9d3132815ff79fa1eba8f15560cd9c7e717c2b1f
GodnessBuddy <marvin.ruwe@hebk.de>
2022-01-07 01:35:55 +0100

    zu spät

---
+++ java/arrays.java
@@ -0,0 +1,13 @@
+public class arrays {
+    public static void main(String[] args) {
+        
+        String[] names = new String[3];//die größe eines Arrays wird immer bei der initialisirung fest
+
+        names[0] = "Hendrick";
+        names[1] = "Paul";
+        names[2] = "Fabienne";
+        
+        System.out.println(names[1]);
+
+    }
+}
