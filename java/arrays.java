f2b46e5b8bc9a8f7f3a57166a7154710df17513f
GodnessBuddy <marvin.ruwe@hebk.de>
2022-01-10 08:53:06 +0100

    folge 13

--- java/arrays.java
+++ java/arrays.java
@@ -1,13 +1,18 @@
 public class arrays {
     public static void main(String[] args) {
         
-        String[] names = new String[3];//die größe eines Arrays wird immer bei der initialisirung fest
-
+        String[] names = new String[3];//die größe eines Arrays wird immer bei der initialisirung fest gelegt
+        
         names[0] = "Hendrick";
         names[1] = "Paul";
         names[2] = "Fabienne";
-        
+        /*
         System.out.println(names[1]);
+        */
+
+        for(int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
 
     }
 }
