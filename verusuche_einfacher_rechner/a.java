e4819986011b68c633bf53457ac4d80b7e2fd19d
GodnessBuddy <marvin.ruwe@hebk.de>
2022-01-17 09:54:23 +0100

    5a arbeit

---
+++ verusuche_einfacher_rechner/a.java
@@ -0,0 +1,17 @@
+package verusuche_einfacher_rechner;
+import java.util.Scanner;
+
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("bitte Zahl eingeben");
+        Scanner sc = new Scanner(System.in);
+        int j = sc.nextInt();
+        for(int i = 1; i <= j; i++){
+            String hallo = "Moin";
+            System.out.println(hallo);
+        }
+   
+    
+}
+}
