9929673486458572f1684ce0d5e59ca0216ee8ec
GodnessBuddy <marvin.ruwe@hebk.de>
2022-01-17 09:55:01 +0100

    etwas

--- verusuche_einfacher_rechner/Rechner.java
+++ verusuche_einfacher_rechner/Rechner.java
@@ -1,7 +1,33 @@
 package verusuche_einfacher_rechner;
+import java.util.Scanner;
 
 public class Rechner {
     public static void main(String[] args) {
+        Scanner rechenform = new Scanner(System.in);
+        System.out.println("Möchtest du Plus, Minus, Mal oder Geteilt rechnen?");
+
+        String rechenart = rechenform.nextLine();
+        Plus();
+
+
         
     }
+
+    public static int Plus(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static int Minus(int number1, int number2) {
+        return number1 - number2;
+    }
+
+    public static int Mal(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public static int Geteilt(int number1, int number2) {
+        return number1 / number2;
+    }
+
+    
 }
---
+++ verusuche_einfacher_rechner/test_datei.java
@@ -0,0 +1,3 @@
+package verusuche_einfacher_rechner;
+
+
